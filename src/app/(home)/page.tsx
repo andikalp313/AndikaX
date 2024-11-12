c7cba0af18373cdfce9551e41a915986eec246e9
andikalp313 <andikaluhurpambudi@gmail.com>
2024-11-12 14:42:23 +0700

    firt commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,48 +1,12 @@
-import AboutSection from "@/components/aboutsection/AboutSection";
-import BlogList from "@/components/bloglist/BlogList";
+import React from "react";
+import Home from "../features/servicepage/page";
 
-import FooterSection from "@/components/footer/footer";
-import HeroSection from "@/components/Jumbotron/page";
-
-import PaginationSection from "@/components/pagination/PaginationSection";
-import TestiSection from "@/components/testimoni/testimoni";
-import { Pagination } from "@/components/ui/pagination";
-import { getEntries } from "@/lib/contentful";
-
-import Image from "next/image";
-
-interface HomeProps {
-  searchParams: { [key: string]: string };
-}
-
-export default async function Home({ searchParams }: HomeProps) {
-  const blogs = await getEntries(Number(searchParams.page) || 1);
-
-  if (!blogs) {
-    return;
-  }
-
+const HomeMain = () => {
   return (
     <div>
-      <div className="flex items-center justify-center mx-auto">
-        <HeroSection />
-      </div>
-      <div className="flex items-center justify-center mx-auto">
-        <AboutSection />
-      </div>
-      <div>
-        <BlogList blogs={blogs.data} />
-      </div>
-      <div>
-        <PaginationSection limit={blogs.meta.limit} total={blogs.meta.total} />
-      </div>
-      <div>
-        <TestiSection />
-      </div>
-
-      <div>
-        <FooterSection />
-      </div>
+      <Home />
     </div>
   );
-}
+};
+
+export default HomeMain;
